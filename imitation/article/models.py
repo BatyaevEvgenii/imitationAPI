670f38e37617bfab5086fac5d60081095e68fd07
eugenesan <eugenesan@ya.ru>
2020-01-23 17:24:45 +0300

    all in

--- imitation/article/models.py
+++ imitation/article/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 # модель авторов
 class Author(models.Model):
     name = models.CharField(max_length=255)
@@ -16,7 +17,8 @@ class Article(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     body = models.TextField()
-    author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
+    # author = models.ForeignKey('Author', related_name='articles', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', related_name='articles', on_delete=models.PROTECT)
 
     # удобочитаемое отображение объектов Article
     def __str__(self):
--- imitation/article/views.py
+++ imitation/article/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
-# попробуем через APIView
+# пробуем через APIView
 from rest_framework.views import APIView
 
 from .models import Article
@@ -9,12 +9,40 @@ from .serializer import ArticleSerializer
 
 # Create your views here.
 
-
 class ArticleView(APIView):
-    # просмотр того что есть в статьях
-    def get(self, request):
+    # смотрим что есть в статьях
+    def get(self, request, pk=None):
+        # если запрашиваем по id
+        if pk:
+            article = get_object_or_404(Article.objects.all(), pk=pk)
+            serializer = ArticleSerializer(article)
+            return Response({"article": serializer.data})
         articles = Article.objects.all()
-        # преобразуем данные
+        # преобразуем все данные(many)
         serializer = ArticleSerializer(articles, many=True)
         # return Response({"articles": articles})
         return Response({"articles": serializer.data})
+
+    def post(self, request):
+        article = request.data.get('article')
+        serializer = ArticleSerializer(data=article)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' created successfuly".format(article_saved.title)})
+
+    def put(self, request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        # partial = True для обновления некоторых полей
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+        return Response({"success": "Article '{}' update successfully".format(article_saved.title)})
+
+    def delete(self, request, pk):
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response({"message": "Article with id '{}' has been deleted.".format(pk)}, status=204)
+
+
